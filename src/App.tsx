afeecd39023f8f0c0da659eec304c10fcaed70d0
Lucag101 <114105978+Lucag101@users.noreply.github.com>
2023-03-22 14:19:58 -0600

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import TeamList from './Card';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Mission 12</h2>
-        <img src={logo} className="App-logo" alt="logo" />
+        
+        <Header/>
+        <TeamList/>
+        
+        
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Header() {
+  return (
+    <div >
+      <h1>March Madness</h1>
+      <h3>On this site you will find all the information on college basketball teams </h3>
+    </div>
+  );
+}
+
+export default Header;
